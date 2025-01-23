40b044d048f0dda7613cb5ec4b5a52224bf0784e
khw2002 <hannahkhw@snu.ac.kr>
2025-01-23 16:27:38 +0900

    first commit- basic structure

---
+++ Game.h
@@ -0,0 +1,17 @@
+#include <SDL.h>
+
+class Game {
+    public:
+        Game(); 
+        bool Initialize(); // initialize game
+        void RunLoop(); // run game loop
+        void Shutdown(); // end game
+    private: 
+        // helper functions for game loop
+        void ProcessInput();
+        void UpdateGame();
+        void GenerateOutput();
+
+        SDL_Window* mWindow; // window made by SDL
+        bool mIsRunning; // if game is still running
+};
